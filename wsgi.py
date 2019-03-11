76632b17a14f80d667070d90f02b1e46e29a2431
curtis <curtis@serverascode.com>
2019-03-11 15:37:40 -0400

    add a healthcheck

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from healthcheck import HealthCheck, EnvironmentDump
+
 application = Flask(__name__)
+
+# wrap the flask app and give a heathcheck url
+health = HealthCheck(application, "/healthcheck")
+envdump = EnvironmentDump(application, "/environment")
+
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+# not sure if this is needed or if /healthcheck is magic
+def check_health():
+    return True, "ok"
+
+health.add_check(check_health)
 
 if __name__ == "__main__":
     application.run()
